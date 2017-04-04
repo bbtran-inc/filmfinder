312447e23df95f22435577efcb7790898c8bb62a
Benjamin Tran <benjamin.tran25@gmail.com>
2017-04-04 11:58:25 -0700

    [ FEAT ] include persistent state using localStorage to keep track of favorites

--- client/configureStore.js
+++ client/configureStore.js
@@ -1,12 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { loadState, saveState } from './localStorage/localStorage';
 import rootReducer from './reducers'; // Or wherever you keep your reducers
 
 export default function configureStore() {
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk)));
+  const persistedState = loadState();
+  const store = createStore(rootReducer, persistedState, composeEnhancers(applyMiddleware(thunk)));
   /* eslint-enable */
+
+  store.subscribe(() => {
+    saveState({
+      favoritesList: store.getState().favoritesList,
+    });
+  });
 
   if (module.hot) {
     module.hot.accept('./reducers', () => {
