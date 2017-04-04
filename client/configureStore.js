4a2450cbf9265ac0f93efc2ce17fd38e88909f54
Benjamin Tran <benjamin.tran25@gmail.com>
2017-04-04 11:05:52 -0700

    [ DEV ] add redux dev tools to store configuration

--- client/configureStore.js
+++ client/configureStore.js
@@ -3,7 +3,11 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers'; // Or wherever you keep your reducers
 
 export default function configureStore() {
-  const store = createStore(rootReducer, applyMiddleware(thunk));
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk)));
+  /* eslint-enable */
+
   if (module.hot) {
     module.hot.accept('./reducers', () => {
       const nextReducer = require('./reducers/index');
