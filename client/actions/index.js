2232dc6563282b85a2c1e9aa9f36f34729024188
Benjamin Tran <benjamin.tran25@gmail.com>
2017-04-03 22:32:35 -0700

    [ FEAT ] Add new action creators for adding and removing favorites

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 export const FETCH_MOVIE = 'FETCH_MOVIE';
 export const UPDATE_SEARCH_TERM = 'UPDATE_SEARCH_TERM';
+export const SET_FAVORITE = 'SET_FAVORITE';
+export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
 
 export function updateSearchTerm(searchTerm) {
   return { type: UPDATE_SEARCH_TERM, payload: searchTerm };
@@ -16,4 +18,15 @@ export function searchByTitle(title) {
       dispatch({ type: FETCH_MOVIE, payload: data });
     });
   };
+}
+
+export function setFavorite(title, imdbID, poster, rated) {
+  const moviedata = {};
+  moviedata[title] = { title, imdbID, poster, rated };
+  console.log('TITLE:', title);
+  return { type: SET_FAVORITE, payload: moviedata };
+}
+
+export function removeFavorite(title) {
+  return { type: REMOVE_FAVORITE, payload: title };
 }
