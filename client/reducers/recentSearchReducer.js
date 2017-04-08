84b5a4e69cd45fa812b079796701df7488c45013
Benjamin Tran <benjamin.tran25@gmail.com>
2017-04-08 16:14:57 -0700

    [ FEAT ] Modify reducers to dedupe recent searches before updating store

--- client/reducers/recentSearchReducer.js
+++ client/reducers/recentSearchReducer.js
@@ -3,8 +3,16 @@ import { STORE_RECENT } from '../actions/index';
 export function recentSearchReducer(state = [], action) {
   switch (action.type) {
     case STORE_RECENT:
-    return [...state, action.payload];
+    return dedupFaves(state, action);
     default:
     return state;
   }
+}
+
+function dedupFaves(prevState, action) {
+  const newState = [...prevState];
+  if (!prevState.includes(action.payload)) {
+    newState.push(action.payload);
+  }
+  return newState;
 }
