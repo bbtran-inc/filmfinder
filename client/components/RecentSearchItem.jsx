61102814f0edc6ef179285f14ce381f0bf5a811e
Benjamin Tran <benjamin.tran25@gmail.com>
2017-04-08 01:11:44 -0700

    [ BUGFIX ] Add action creator to recent search component to update with the correct searchPhrase

--- client/components/RecentSearchItem.jsx
+++ client/components/RecentSearchItem.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { searchByTitle, updateSearchTerm } from '../actions/index';
+class RecentSearchItem extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(event) {
+    event.preventDefault();
+    this.props.searchByTitle(this.props.searchPhrase);
+    this.props.updateSearchTerm(this.props.searchPhrase);
+  }
 
-export const RecentSearchItem = (props) => {
-  return (
-    <li><a href="/" onClick={props.onSomeEvent}><span>{props.searchPhrase}</span></a></li>
-  );
-};
+  render() {
+    return (
+      <li><a href="/" onClick={this.handleClick}><span>{this.props.searchPhrase}</span></a></li>);
+  }
+}
 
 RecentSearchItem.propTypes = {
-  onSomeEvent: React.PropTypes.func,
+  updateSearchTerm: React.PropTypes.func,
+  searchByTitle: React.PropTypes.func,
   searchPhrase: React.PropTypes.string,
 };
 
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ searchByTitle, updateSearchTerm }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(RecentSearchItem);
--- client/containers/RecentSearchList.jsx
+++ client/containers/RecentSearchList.jsx
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import * as actions from '../actions';
-import { RecentSearchItem } from '../components/RecentSearchItem';
+import RecentSearchItem from '../components/RecentSearchItem';
 
 class RecentSearchList extends Component {
-  constructor(props) {
-    super(props);
-    this.handleEvent = this.handleEvent.bind(this);
-  }
-  handleEvent(item) {
-    this.props.searchByTitle(item);
-  }
   render() {
     const resultList = this.props.recentSearches.map((item) => {
-      return <RecentSearchItem searchPhrase={item} onSomeEvent={this.handleEvent(item)} />;
-    });
+      return <RecentSearchItem searchPhrase={item} />;
+    }, this);
     return (
       <ul className="results">
         <li><a href="/"><span>YOUR RECENT SEARCHES</span></a></li>
@@ -30,7 +22,6 @@ function mapStateToProps({ recentSearches }) {
 
 RecentSearchList.propTypes = {
   recentSearches: React.PropTypes.array,
-  searchByTitle: React.PropTypes.func,
 };
 
-export default connect(mapStateToProps, actions)(RecentSearchList);
+export default connect(mapStateToProps)(RecentSearchList);
